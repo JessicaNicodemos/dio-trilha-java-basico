6a6e170bea914dc1b20b6b8b02020cf933f6ec7b
Jessica <jessicanicodemos@gmail.com>
2022-11-15 12:04:25 -0300

    atualização aula 4 de operadores

---
+++ tipos-variaves/src/operadores.java
@@ -0,0 +1,29 @@
+import javax.swing.Spring;
+
+public class operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+        int numero = 5;
+
+            numero = - numero; 
+
+            System.out.println(numero);
+
+            numero = numero * -1;
+            System.out.println(numero);
+
+ 
+        int numero1 = 5;
+
+            System.out.println( ++ numero1);
+            System.out.println(numero1);
+
+    boolean variavel = true;
+    System.out.println(!variavel);
+    System.out.println(variavel);
+}
+    
+    }
