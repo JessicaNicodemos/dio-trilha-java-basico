5380ae7315de1d3e830750de35c8a7b03a44717b
Jessica <jessicanicodemos@gmail.com>
2022-11-17 16:31:28 -0300

    Atualização sobre exercicios de arrays

---
+++ estudos-java/Exercícios-loops-e-arrays/Arrays/Ex3_NumerosAleatorios.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+/*
+Faça um Programa que leia 20 números inteiros aleatórios (entre 0 e 100) armazene-os num vetor.
+Ao final mostre os números e seus sucessores.
+*/
+public class Ex3_NumerosAleatorios {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int [20];
+
+        for(int i = 0; i < numerosAleatorios.length; i++) {
+            // continuar em 04 min
+        }
+    }
+
+}
--- estudos-java/Exercícios-loops-e-arrays/Exercícios-loops-e-arrays/Arrays/Ex3_NumerosAleatorios.java
+++
@@ -1,7 +0,0 @@
-/*
-Faça um Programa que leia 20 números inteiros aleatórios (entre 0 e 100) armazene-os num vetor.
-Ao final mostre os números e seus sucessores.
-*/
-public class Ex3_NumerosAleatorios {
-    
-}
